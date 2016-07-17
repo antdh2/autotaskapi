9c08caa570d602ad021798d4fab685ea1b9b5eb4
Ant Horner <antdh2@gmail.com>
2016-07-17 23:26:27 +0100

    Added account name resolver

--- autotask_web_app/views.py
+++ autotask_web_app/views.py
@@ -4,36 +4,49 @@ from django.shortcuts import render_to_response
 import atws
 
 
+
+# First we must connect to autotask using valid credentials
+at = atws.connect(username='ant.horner@eye-techit.com',password='Mnschnaap1!')
+
 # Create your views here.
 def index(request):
-    # First we must connect to autotask using valid credentials
-    at = atws.connect(username='ant.horner@eye-techit.com',password='Mnschnaap1!')
+    # Once an account name/id is entered
+    if request.method == "POST":
+        # map account_id to the inputted value
+        account_id = request.POST['input-account-id']
+        # then get autotask account using that ID
+        # accounts = get_account(account_id)
+
+        accounts = resolve_account_name(account_id)
+
+    else:
+        account_id = None
+        accounts = None
+
+
+
+    return render(request, 'index.html', {"account_id": account_id, "accounts": accounts})
+
+
+def get_account(account_id):
     # Then we need to grab a query object using autotask wrapper
     query = atws.Query('Account')
     # Then filter what we want the query object to grab using SQL
-    query.WHERE('id',query.Equals,29774062)
+    query.WHERE('id',query.Equals,account_id)
     # Assign the generator from query object to a list which we can interact with
     accounts = at.query(query).fetch_one()
-    # Loop through tuples in accounts to find the field AccountName and check it's equal to user input
-    # Then print that
-    for field, value in accounts:
-        if field == "AccountName" and value == "test":
-            account_name_field = field
-            account_name_value = value
-        if field == "id":
-            account_id_field = field
-            account_id_value = str(value)
+    return accounts
 
-
+def get_tickets_for_account(account_id):
     tquery = atws.Query('Ticket')
-    tquery.WHERE('AccountID',query.Equals,account_id_value)
+    tquery.WHERE('AccountID',query.Equals,account_id)
     tickets = at.query(tquery).fetch_one()
 
 
-    if request.method == "POST":
-        # do stuff with a form input
-        user_account_id_input = request.POST['input-account-id']
-    else:
-        user_account_id_input = ""
-
-    return render(request, 'index.html', {"accounts": accounts, "tickets": tickets, "user_account_id_input": user_account_id_input})
+def resolve_account_name(string):
+    aquery = atws.Query('Account')
+    aquery.WHERE('AccountName',aquery.Equals,string)
+    accounts = at.query(aquery).fetch_one()
+    for field, value in accounts:
+        if field == "AccountName" and value == string:
+            return accounts
